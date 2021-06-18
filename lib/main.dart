22e060b900d42939214d18f5bc1c961ca9f3fdc3
rishavm22 <rishavm42@gmail.com>
2021-06-18 23:10:00 +0530

    imports manage

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,19 @@
 import 'package:flutter/material.dart';
-// import 'package:imd_assignment/models/modeState.dart';
 import 'package:imd_assignment/pages/homePage.dart';
 import 'package:imd_assignment/provider/homePageProvider.dart';
 import 'package:imd_assignment/provider/modeProvider.dart';
 import 'package:provider/provider.dart';
 
 void main() {
-  runApp( MultiProvider(
-    providers: [
-      ChangeNotifierProvider(create: (_) => HomePageProvider()),
-      ChangeNotifierProvider(create: (_) => ModeProvider()),
-    ],
-    child: MyApp(),
-  ),);
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => HomePageProvider()),
+        ChangeNotifierProvider(create: (_) => ModeProvider()),
+      ],
+      child: MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
